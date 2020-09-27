a3cd4e8a78a80d502aa09e787b4fe96d46de2cfd
Camilo Gomez <camilogomezmusic@gmail.com>
2020-09-26 23:24:32 -0400

    Add MapWrapper component

---
+++ client/src/components/MapWrapper.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useRef } from 'react';
+import WorldMap from '../d3/WorldMap'
+
+
+import { Row, Col, Card } from 'react-bootstrap'
+import WorldMap from '../d3/DrawWorldMap';
+
+function WorldMapWrapper(props) {
+  
+  // create a hook to refer to our svg element and set the map
+  const svgRef = useRef();
+  const [map, setMap] = useState(null);
+
+  //data will come from the top level, we'll format it and pass it to both visual and sound components
+  const dummyData = [1,2,3,4,5];
+
+  useEffect(() => {
+    // DrawWorldMap(svgRef.current);
+    if (!map)
+      setMap(new WorldMap(svgRef.current))
+    else
+      map.update(dummyData)
+  }, [map, dummyData] ) 
+
+  return (
+    <Row>
+      <Col sm="12" md="9" lg="9" xl="9" >
+          <Card className="map-container">
+            <svg ref={svgRef} className="map-vis"></svg>
+          </Card>
+      </Col>
+    </Row>
+  )
+}
+
+export default WorldMapWrapper;
+
+
+
