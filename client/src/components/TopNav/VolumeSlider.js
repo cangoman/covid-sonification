43bfefc65e9cc3e31d4a6aae403149f39a475539
Camilo Gomez <camilogomezmusic@gmail.com>
2020-09-27 20:29:30 -0400

    Add hooks to control volume/mute properties. Add icons for different volume levels

--- client/src/components/TopNav/VolumeSlider.js
+++ client/src/components/TopNav/VolumeSlider.js
@@ -1,17 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faVolumeUp } from '@fortawesome/free-solid-svg-icons'
+import { faVolumeUp, faVolumeDown, faVolumeOff } from '@fortawesome/free-solid-svg-icons'
 import "./VolumeSlider.scss"
 
 
 function VolumeSlider(props) {
-  const [volume, setVolume] = useState(null);
+  const [volume, setVolume] = useState(50);
+  const [mute, setMute] = useState(false)
   
+  useEffect( () => {
+
+  }, [volume])
+
+  const volumeIcon = () => {
+    if (volume < 1 || mute)
+      return faVolumeOff;
+    return volume < 50 ? faVolumeDown : faVolumeUp;
+  }
   
   return (
     <div className="volume-control">
-      <FontAwesomeIcon icon={faVolumeUp} size="2x" className={"volume-icon"} />
-      <input type="range"></input>
+      <FontAwesomeIcon 
+        icon={volumeIcon()} 
+        size="2x"  
+        className={"volume-icon"} 
+        onClick={() => setMute(!mute)}
+      />
+  
+      <input 
+        id="volume"
+        type="range"
+        min={0}
+        max={100}
+        onChange={ e => {
+          setVolume(e.currentTarget.value);
+        }}
+        disabled={mute} 
+      ></input>
     </div>
   )
 }
