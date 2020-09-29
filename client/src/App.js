d6aa257e561f802b362b5abfc7522bf31679f757
Sam <super8989@gmail.com>
2020-09-28 21:00:17 -0400

    Move TopNav component out of router to display in all the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.scss';
 
@@ -14,43 +14,42 @@ import Login from './components/Login/Login';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios'
+import axios from 'axios';
 
-const BASE_URL = 'https://covid19-api.org/api/timeline/'
+const BASE_URL = 'https://covid19-api.org/api/timeline/';
 
 function App() {
 	const { state, dispatch } = useApplicationData();
 
-	const [timelineData, setTimelineData] = useState([])
+	const [timelineData, setTimelineData] = useState([]);
 
 	//This will need to be selected by the user. and rn it causes a warning on the browser
-	const query = ['colombia', 'canada', "mexico", 'brazil', "france"]
+	const query = ['colombia', 'canada', 'mexico', 'brazil', 'france'];
 
 	useEffect(() => {
 		if (state.countries) {
 			for (const country of query) {
-				const regexp = new RegExp(country, 'i')
-				const countryInfo = state.countries.find( element => element.name.match(regexp))
+				const regexp = new RegExp(country, 'i');
+				const countryInfo = state.countries.find((element) =>
+					element.name.match(regexp)
+				);
 				// console.log(countryInfo)
 				if (countryInfo) {
 					axios({
 						method: 'GET',
-						url: `${BASE_URL}${countryInfo.alpha2Code}`
-					})
-					.then(response => {
-						const countryObject = {countryInfo, data: response.data}
-						setTimelineData( timelineData => [...timelineData, countryObject] )
-					})
+						url: `${BASE_URL}${countryInfo.alpha2Code}`,
+					}).then((response) => {
+						const countryObject = { countryInfo, data: response.data };
+						setTimelineData((timelineData) => [...timelineData, countryObject]);
+					});
 				}
 			}
 		}
 	}, [state.countries]);
 
-
-
-
 	return (
 		<div className='App'>
+			<TopNav />
 			<Router>
 				<Switch>
 					<Route exact path='/register'>
@@ -62,7 +61,6 @@ function App() {
 					</Route>
 
 					<Route exact path='/'>
-						<TopNav />
 						<Container className='app-container' fluid>
 							<Row>
 								<Col xl={10}>
@@ -76,10 +74,8 @@ function App() {
 					</Route>
 				</Switch>
 			</Router>
-			</div>
-		)
-
-	
+		</div>
+	);
 }
 
 export default App;
