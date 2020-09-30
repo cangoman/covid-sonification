62c423164ffe7fbb4f44b93471011bc7bb55bfe1
Sam <super8989@gmail.com>
2020-09-29 21:43:47 -0400

    Style register page

---
+++ client/src/components/Register/RegisterButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function RegisterButton() {
+	return (
+		<Button href='/register' variant='outline-light' className='ml-3'>
+			Register
+		</Button>
+	);
+}
+
+export default RegisterButton;
