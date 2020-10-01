6a9ce19c8adb72ffa804a40afd5d59a21d86e842
Sam <super8989@gmail.com>
2020-09-30 21:08:33 -0400

    Update Sidebar DataCard component

--- client/src/components/SideBar/DataCard.js
+++ client/src/components/SideBar/DataCard.js
@@ -3,18 +3,14 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 function DataCard(props) {
-
 	return (
-		<Card>
+		<Card bg='dark' text='white'>
+			<Card.Header>{props.date}</Card.Header>
 			<Card.Body>
-				<Card.Title>{props.date}</Card.Title>
-				<Card.Subtitle className='mb-2 text-muted'></Card.Subtitle>
 				<Card.Text>
 					Some quick example text to build on the card title and make up the
 					bulk of the card's content.
 				</Card.Text>
-				<Card.Link href='#'>Card Link</Card.Link>
-				<Card.Link href='#'>Another Link</Card.Link>
 			</Card.Body>
 		</Card>
 	);
