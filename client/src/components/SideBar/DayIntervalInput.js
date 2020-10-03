a5381a525e41ed5c900f2166a8c2d37c9d571245
Camilo Gomez <camilogomezmusic@gmail.com>
2020-10-03 16:08:14 -0400

    Latest changes into branch

---
+++ client/src/components/SideBar/DayIntervalInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './DayIntervalInput.scss';
+
+function DayIntervalInput(props) {
+	return (
+		<div className='day-interval-input__container'>
+			<div className='day-interval-input__container--left'>
+				<label htmlFor='day-duration'>Day interval</label>
+			</div>
+			<div className='day-interval-input__container--right'>
+				<input
+					name='day-duration'
+					type='number'
+					max='5000'
+					step='0.1'
+					value={props.interval / 1000}
+					placeholder='seconds'
+					onChange={(e) => props.setInterval(e.currentTarget.value * 1000)}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default DayIntervalInput;
