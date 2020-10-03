643d142e884b01bcc90a25c57515fafee85267d7
Sam <super8989@gmail.com>
2020-10-03 18:26:25 -0400

    Style sidebar components

--- client/src/components/SideBar/CountrySelector.js
+++ client/src/components/SideBar/CountrySelector.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import MultiSelect from 'react-multi-select-component';
 import { Button } from 'react-bootstrap';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faCheckSquare,
+	faWindowClose,
+} from '@fortawesome/free-regular-svg-icons';
+
 import './CountrySelector.scss';
 
 function CountrySelector(props) {
@@ -26,17 +32,30 @@ function CountrySelector(props) {
 				/>
 			</div>
 			<div className='country-selector__container--right'>
-				<Button variant='outline-light' onClick={() => props.setSelected([])}>
-					Clear Selection
-				</Button>
-				<Button
+				{/* <Button
 					variant='outline-light'
 					onClick={() => {
 						props.setQuery(props.selected.map((country) => country.label));
 					}}
 				>
 					Confirm Selection
-				</Button>
+				</Button> */}
+				<FontAwesomeIcon
+					icon={faCheckSquare}
+					onClick={() => {
+						props.setQuery(props.selected.map((country) => country.label));
+					}}
+					className='country-selector-confirm'
+				/>
+
+				{/* <Button variant='outline-light' onClick={() => props.setSelected([])}>
+					Clear Selection
+				</Button> */}
+				<FontAwesomeIcon
+					icon={faWindowClose}
+					onClick={() => props.setSelected([])}
+					className='country-selector-cancel'
+				/>
 			</div>
 		</div>
 	);
