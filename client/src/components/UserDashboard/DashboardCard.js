d9169ce4ede3b240c0853e1a81d8b47c4799cdef
Sam <super8989@gmail.com>
2020-10-05 21:41:46 -0400

    Style user dashboard page

--- client/src/components/UserDashboard/DashboardCard.js
+++ client/src/components/UserDashboard/DashboardCard.js
@@ -3,27 +3,78 @@ import React from 'react';
 import './DashboardCard.scss';
 
 function DashboardCard(props) {
-	console.log(props.state)
-	
+	console.log(props.state);
+
 	return (
 		<div className='dashboard-card__container'>
 			<div className='dashboard-card__title'>
 				<h1>Composition</h1>
 			</div>
 			<div className='dashboard-card__table'>
-				<div className='dashboard-card__table--row'>
-					<div className='dashboard-card__table--row-left row-header'>
-						Title
+				<>
+					<div className='dashboard-card__table--row-header'>
+						<div className='dashboard-card__table--row-header-left'>Title</div>
+						<div className='dashboard-card__table--row-header-right'>
+							Created On
+						</div>
 					</div>
-					<div className='dashboard-card__table--row-right row-header'>
-						Created On
+					<div className='dashboard-card__table--row-text'>
+						<a
+							href={props.link}
+							className='dashboard-card__table--row-text-left'
+						>
+							{props.title}
+						</a>
+						<div className='dashboard-card__table--row-text-right'>
+							{props.createdOn.substr(0, 10)}
+						</div>
 					</div>
-				</div>
-				<div className='dashboard-card__table--row'>
-					<a href={props.link} className='dashboard-card__table--row-left'>{props.title}</a>
-					<div className='dashboard-card__table--row-right'>{props.createdOn.substr(0,10)}</div>
-				</div>
+				</>
 
+				<>
+					<div className='dashboard-card__table--row-header'>
+						<div className='dashboard-card__table--row-header-left'>
+							Date Range
+						</div>
+						<div className='dashboard-card__table--row-header-right'></div>
+					</div>
+					<div className='dashboard-card__table--row-text'>
+						<div className='dashboard-card__table--row-text-left'>
+							{props.startDate} ~ {props.endDate}
+						</div>
+						<div className='dashboard-card__table--row-text-right'></div>
+					</div>
+				</>
+
+				<>
+					<div className='dashboard-card__table--row-header'>
+						<div className='dashboard-card__table--row-header-left'>
+							Day Interval (ms)
+						</div>
+						<div className='dashboard-card__table--row-header-right'></div>
+					</div>
+					<div className='dashboard-card__table--row-text'>
+						<div className='dashboard-card__table--row-text-left'>
+							{props.interval}
+						</div>
+						<div className='dashboard-card__table--row-text-right'></div>
+					</div>
+				</>
+
+				<>
+					<div className='dashboard-card__table--row-header'>
+						<div className='dashboard-card__table--row-header-left'>
+							Selected Countries
+						</div>
+						<div className='dashboard-card__table--row-header-right'></div>
+					</div>
+					<div className='dashboard-card__table--row-text'>
+						<div className='dashboard-card__table--row-text-left'>
+							{props.countries.join(', ')}
+						</div>
+						<div className='dashboard-card__table--row-text-right'></div>
+					</div>
+				</>
 			</div>
 		</div>
 	);
